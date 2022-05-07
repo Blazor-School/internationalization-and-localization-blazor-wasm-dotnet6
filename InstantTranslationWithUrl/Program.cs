3b6ccabf3a7529be6d242dd2c5d7b6235e8412df
Red Wei <hevilhuy2@gmail.com>
2022-05-07 20:09:21 +0700

    Format code.

--- InstantTranslationWithUrl/Program.cs
+++ InstantTranslationWithUrl/Program.cs
@@ -8,8 +8,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddHttpClient("InternalHttpClient", httpClient => httpClient.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new (builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("InternalHttpClient", httpClient => httpClient.BaseAddress = new (builder.HostEnvironment.BaseAddress));
 builder.Services.AddScoped(typeof(IStringLocalizer<>), typeof(BlazorSchoolStringLocalizer<>));
 builder.Services.AddScoped<BlazorSchoolCultureProvider>();
 builder.Services.AddScoped<BlazorSchoolResourceMemoryStorage>();
